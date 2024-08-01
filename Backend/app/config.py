a60966bebeb5f8f9c09f783d1f0f42405086b60b
Nitika2334 <nitikaruhal1503@gmail.com>
2024-08-01 19:50:40 +0530

    modified config file

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = 'mysecret'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:1234@localhost:5432/mini_blog'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql+psycopg2://postgres:1525@localhost:5432/Blog_app')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'your_jwt_secret_key')
+
